7de8fe602b9a005c011bebf6052817ccc6e5f793
Dan <danielpowell12@msn.com>
2016-09-16 14:18:33 -0600

    Ensures consistent ordering of tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -10,11 +10,24 @@ RSpec.describe TasksController, type: :controller do
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
     end
+    it 'should list tasks based upon id' do
+      task1 = FactoryGirl.create(:task)
+      task2 = FactoryGirl.create(:task)
+
+      get :index
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      task1.update_attributes(title: 'Something else')
+      response_ids = response_value.collect do |task|
+        task['id']
+      end
+
+      expect(response_ids).to eq([task1.id, task2.id])
+    end
   end
-  describe "tasks#update" do
-    it "should allow tasks to be marked as done" do
+  describe 'tasks#update' do
+    it 'should allow tasks to be marked as done' do
       task = FactoryGirl.create(:task, done: false)
-      put :update, params: {id: task.id, task: { done: true }}
+      put :update, params: { id: task.id, task: { done: true } }
       expect(response).to have_http_status(:success)
       task.reload
       expect(task.done).to eq(true)
